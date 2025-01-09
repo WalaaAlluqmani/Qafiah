ebb8c24005d1c55c4869610d369c2af720c3760d
LeenaAliAlymani <laqymani@gmail.com>
2025-01-09 18:46:38 +0300

    niiicee

--- main.py
+++ main.py
@@ -23,6 +23,10 @@ async def generate_poetry(request: Request):
     return templates.TemplateResponse("poetry_generation_poets.html", {"request": request})
 
 
+@app.get("/select-classification", response_class=HTMLResponse)
+async def generate_poetry(request: Request):
+    return templates.TemplateResponse("poetry_classification.html", {"request": request})
+
 
 @app.get("/generate-poetry", response_class=HTMLResponse)
 async def generate_poetry(request: Request):
