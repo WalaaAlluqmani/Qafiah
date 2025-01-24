6d64ef1eb4567ecbf94ac13d45c4b012117743d6
Walaa <wloow159@gmail.com>
2025-01-24 19:29:38 +0300

    Update

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ templates = Jinja2Templates(directory="templates")
 
 # --------------------------------AraGPT-2----------------------------------------------
 
-model_path = "models/AraGPT-mutnabi"
-model = GPT2LMHeadModel.from_pretrained(model_path)
-tokenizer = GPT2TokenizerFast.from_pretrained(model_path)
-model.eval()
+# # model_path = "models/AraGPT-mutnabi"
+# model = GPT2LMHeadModel.from_pretrained(model_path)
+# tokenizer = GPT2TokenizerFast.from_pretrained(model_path)
+# model.eval()
 
-generation_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer)
+# generation_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer)
 
 # --------------------------------pages-----------------------------------------------
 
@@ -40,29 +40,30 @@ async def generate_poetry(request: Request, type: str = Form(...)):
     line = "كسوتني حلة تبلى محاسنها"
     if type == "مدح":
         generatedpoem = generatePoem_BasedonGenre(line)
-    elif type == "ابو الطيب المتنبي":
-        generatedpoem = generatePoem_BasedonPoet(line)
+    # elif type == "ابو الطيب المتنبي":
+    #     generatedpoem = generatePoem_BasedonPoet(line)
     else:
         raise HTTPException(status_code=400, detail="Invalid model type")
     
     return templates.TemplateResponse("poetry_display.html", {"request": request,"generated_poem": generatedpoem, "type": type})
 
 #-------------------------------------------------------------------------------------
-def generatePoem_BasedonPoet(prompt: str) -> str:
+# def generatePoem_BasedonPoet(prompt: str) -> str:
      
-    generated_poem = generation_pipeline(
-      prompt,
-      pad_token_id=tokenizer.eos_token_id,
-      num_beams=6,
-      max_length=50,
-      top_p=0.9,
-      temperature=0.9,
-      repetition_penalty = 3.0,
-      no_repeat_ngram_size = 3,
-      early_stopping=True,
-      truncation=True
-    )[0]['generated_text']
-    return generated_poem
+#     generated_poem =" "
+#     # generation_pipeline(
+#     # #   prompt,
+    #   pad_token_id=tokenizer.eos_token_id,
+    #   num_beams=6,
+    #   max_length=50,
+    #   top_p=0.9,
+    #   temperature=0.9,
+    #   repetition_penalty = 3.0,
+    #   no_repeat_ngram_size = 3,
+    #   early_stopping=True,
+    #   truncation=True
+    # )[0]['generated_text']
+    # return generated_poem
 
 #-------------------------------------------------------------------------------------
 def generatePoem_BasedonGenre(prompt: str) -> str:
