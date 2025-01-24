061901c071f40737385419fbcd64d84d4e5821ec
Walaa <wloow159@gmail.com>
2025-01-24 08:31:17 +0300

    Update Files

--- static/JavaScript/index.js
+++ static/JavaScript/index.js
@@ -1,7 +1,7 @@
-
 function scrollToSection() {
     const section = document.getElementById('section2');
     if (section) {
         section.scrollIntoView({ behavior: 'smooth' }); // انتقال سلس إلى القسم
     }
 }
+
