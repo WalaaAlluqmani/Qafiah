a43e39f795df879be585eec3c6ed590a23c498eb
Walaa <wloow159@gmail.com>
2025-01-26 05:37:24 +0300

    Update Files

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 from fastapi import FastAPI, HTTPException, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -5,13 +6,13 @@ from fastapi.staticfiles import StaticFiles
 from transformers import GPT2TokenizerFast, GPT2LMHeadModel, pipeline
 import random
 
-# ----------------------------------------------------------------------------------
+logging.basicConfig(level=logging.INFO)
+
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-# --------------------------------AraGPT2-mutnabi ----------------------------------------------
-
+# Models setup
 model_path = "WalaaAlluqmani/mutnabi_model"
 model_mutnabi = GPT2LMHeadModel.from_pretrained(model_path)
 tokenizer_mutnabi = GPT2TokenizerFast.from_pretrained(model_path)
@@ -19,90 +20,82 @@ model_mutnabi.eval()
 
 generation_mutnabi_pipeline = pipeline("text-generation", model=model_mutnabi, tokenizer=tokenizer_mutnabi)
 
-
-# --------------------------------AraGPT2-madih ----------------------------------------------
-
-model_name= "WalaaAlluqmani/madih_model"
+model_name = "WalaaAlluqmani/madih_model"
 model_madih = GPT2LMHeadModel.from_pretrained(model_name)
 tokenizer_madih = GPT2TokenizerFast.from_pretrained(model_name)
 model_madih.eval()
 
 generation_madih_pipeline = pipeline("text-generation", model=model_madih, tokenizer=tokenizer_madih)
 
-# --------------------------------pages-----------------------------------------------
-
+# Routes
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/select-poet", response_class=HTMLResponse)
-async def generate_poetry(request: Request):
+async def select_poet(request: Request):
     return templates.TemplateResponse("poetry_generation_poets.html", {"request": request})
 
 @app.get("/select-genre", response_class=HTMLResponse)
-async def generate_poetry(request: Request):
+async def select_genre(request: Request):
     return templates.TemplateResponse("poetry_generation_genre.html", {"request": request})
 
 @app.post("/generate-poetry", response_class=HTMLResponse)
 async def generate_poetry(request: Request, type: str = Form(...)):
-
-    prompt = prompt(type)
+    prompt_text = get_prompt(type)
 
     if type == "مدح":
-        generatedpoem = generatePoem_BasedonGenre(prompt)
+        generated_poem = generate_poem_based_on_genre(prompt_text)
     elif type == "أبو الطيب المتنبي":
-        generatedpoem = generatePoem_BasedonPoet(prompt)
+        generated_poem = generate_poem_based_on_poet(prompt_text)
     else:
         raise HTTPException(status_code=400, detail="Invalid model type")
     
-    return templates.TemplateResponse("poetry_display.html", {"request": request,"generated_poem": generatedpoem, "type": type})
+    logging.info(f"Generated poem for type '{type}': {generated_poem}")
+    
+    return templates.TemplateResponse("poetry_display.html", {"request": request, "generated_poem": generated_poem, "type": type})
 
-#-------------------------------------------------------------------------------------
-def generatePoem_BasedonPoet(prompt: str) -> str:
-     
+# Poetry generation functions
+def generate_poem_based_on_poet(prompt: str) -> str:
     generated_poem = generation_mutnabi_pipeline(
-      prompt,
-      pad_token_id=tokenizer_mutnabi.eos_token_id,
-      num_beams=6,
-      max_length=50,
-      top_p=0.95,
-      temperature=0.7,
-      repetition_penalty = 3.0,
-      no_repeat_ngram_size = 3,
-      early_stopping=True,
-      truncation=True
+        prompt,
+        pad_token_id=tokenizer_mutnabi.eos_token_id,
+        num_beams=6,
+        max_length=50,
+        top_p=0.95,
+        temperature=0.7,
+        repetition_penalty=3.0,
+        no_repeat_ngram_size=3,
+        early_stopping=True,
+        truncation=True
     )[0]['generated_text']
     return generated_poem
 
-#-------------------------------------------------------------------------------------
-def generatePoem_BasedonGenre(prompt: str) -> str:
+def generate_poem_based_on_genre(prompt: str) -> str:
     generated_poem_madih = generation_madih_pipeline(
-      prompt,
-      pad_token_id=tokenizer_madih.eos_token_id,
-      num_beams=6,
-      max_length=50,
-      top_p=0.95,
-      temperature=0.7,
-      repetition_penalty = 3.0,
-      no_repeat_ngram_size = 3,
-      early_stopping=True,
-      truncation=True
+        prompt,
+        pad_token_id=tokenizer_madih.eos_token_id,
+        num_beams=6,
+        max_length=50,
+        top_p=0.95,
+        temperature=0.7,
+        repetition_penalty=3.0,
+        no_repeat_ngram_size=3,
+        early_stopping=True,
+        truncation=True
     )[0]['generated_text']
     return generated_poem_madih
-   
-#-------------------------------------------------------------------------------------
-def prompt(type: str) -> str:
 
+def get_prompt(type: str) -> str:
     if type == "مدح":
-      with open('prompts/random_prompt.txt', 'r', encoding='utf-8') as file:
-        lines = file.readlines()
-        random_line = random.choice(lines).strip()
-        return random_line
+        with open('prompts/random_prompt.txt', 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+            random_line = random.choice(lines).strip()
+            return random_line
     elif type == "أبو الطيب المتنبي":
-      with open('prompts/prompts/prompt_mutnabi.txt', 'r', encoding='utf-8') as file:
-        lines = file.readlines()
-        random_line = random.choice(lines).strip()
-        return random_line
+        with open('prompts/prompts/prompt_mutnabi.txt', 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+            random_line = random.choice(lines).strip()
+            return random_line
     else:
         return "نوع النص غير معروف."
-
